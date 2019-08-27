d326dcbab1de424702a54c48d87d4eaa75281d18
Linda <lindahuang@steelhouse.com>
2019-08-26 22:34:06 -0700

    wip

--- src/main/kotlin/linda/insurance/configuration/BigBrotherConfig.kt
+++ src/main/kotlin/linda/insurance/configuration/BigBrotherConfig.kt
@@ -1,10 +1,12 @@
 package linda.insurance.configuration
 
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties
+@ConfigurationProperties(prefix = "bigbrother")
 class BigBrotherConfig {
 
     lateinit var host: String
